e34ffa59319adcf9494e6a72cad08b523818d295
Austin <austinrdelgado@gmail.com>
2022-01-11 16:44:30 -0500

    Added 2D array of cells for grid

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ let width = Math.floor(window.screen.availWidth / 25);
 const grid = document.getElementById('grid');
 
 // Create grid based off screen size
+let cellArray = new Array(height); // Create 2d array for the grid, probably not best practice
 for (let y = 0; y < height; y++)
 {
     // New table row
     let row = document.createElement('tr');
+    cellArray[y] = new Array(width);
 
     for (let x = 0; x < width; x++)
     {
@@ -18,6 +20,7 @@ for (let y = 0; y < height; y++)
         cell.id = id;
 
         row.appendChild(cell);
+        cellArray[y][x] = cell;
     }
     grid.appendChild(row);
 }
