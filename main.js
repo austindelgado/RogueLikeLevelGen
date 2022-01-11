2273948cea57131749503ad013412cfe94d25d36
Austin <austinrdelgado@gmail.com>
2022-01-10 22:38:25 -0500

    Switched to using createElement for grid

--- main.js
+++ main.js
@@ -5,22 +5,22 @@ let width = Math.floor(window.screen.availWidth / 25);
 const grid = document.getElementById('grid');
 
 // Create grid based off screen size
-let tableHTML = "";
 for (let y = 0; y < height; y++)
 {
     // New table row
-    let currentHTMLRow = `<tr>`;
+    let row = document.createElement('tr');
 
     for (let x = 0; x < width; x++)
     {
         // New col in row
         let id = y * width + x;
-        currentHTMLRow += `<td id=${id}></td>`;
-    }
-    tableHTML += `${currentHTMLRow}</tr>`;
-}
+        let cell = document.createElement('td');
+        cell.id = id;
 
-grid.innerHTML = tableHTML;
+        row.appendChild(cell);
+    }
+    grid.appendChild(row);
+}
 
 // Probably better to create an actual 2D array of the elements, rather than this id system
 for (let x = 0; x < width; x++)
