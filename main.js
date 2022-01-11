06ed4e5c1c6d548960b79c5371683d472ff8d305
Austin <austinrdelgado@gmail.com>
2022-01-10 22:41:13 -0500

    Added test to click on cells

--- main.js
+++ main.js
@@ -16,22 +16,9 @@ for (let y = 0; y < height; y++)
         let id = y * width + x;
         let cell = document.createElement('td');
         cell.id = id;
+        cell.addEventListener('click', () => {cell.classList.add("floor")})
 
         row.appendChild(cell);
     }
     grid.appendChild(row);
-}
-
-// Probably better to create an actual 2D array of the elements, rather than this id system
-for (let x = 0; x < width; x++)
-{
-    let y = 10;
-    let cell = document.getElementById((y-1) * width + x);
-    cell.classList.add("wall");
-
-    cell = document.getElementById(y * width + x);
-    cell.classList.add("floor");
-
-    cell = document.getElementById((y+1) * width + x);
-    cell.classList.add("wall");
 }
