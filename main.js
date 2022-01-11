a09338a8c28e521b8f1fd29b44489224bf6fef88
Austin <austinrdelgado@gmail.com>
2022-01-10 22:34:45 -0500

    Added wall and floor css with test

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
 // Find a way to handle dynamic resizing
-let height = Math.floor((document.documentElement.scrollHeight));
-let width = Math.floor(document.documentElement.scrollWidth);
+let height = Math.floor(window.screen.availHeight / 30);
+let width = Math.floor(window.screen.availWidth / 25);
 
 const grid = document.getElementById('grid');
 
 // Create grid based off screen size
 let tableHTML = "";
-for (let x = 0; x < height; x++)
+for (let y = 0; y < height; y++)
 {
     // New table row
     let currentHTMLRow = `<tr>`;
 
-    for (let y = 0; y < width; y++)
+    for (let x = 0; x < width; x++)
     {
         // New col in row
-        let id = x*width + y;
-        currentHTMLRow += `<td id=${id} >${id}</td>`;
+        let id = y * width + x;
+        currentHTMLRow += `<td id=${id}></td>`;
     }
     tableHTML += `${currentHTMLRow}</tr>`;
 }
 
 grid.innerHTML = tableHTML;
 
-for (let i = 0; i < width; i++)
+// Probably better to create an actual 2D array of the elements, rather than this id system
+for (let x = 0; x < width; x++)
 {
+    let y = 10;
+    let cell = document.getElementById((y-1) * width + x);
+    cell.classList.add("wall");
 
+    cell = document.getElementById(y * width + x);
+    cell.classList.add("floor");
+
+    cell = document.getElementById((y+1) * width + x);
+    cell.classList.add("wall");
 }
