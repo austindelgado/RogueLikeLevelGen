bbb23c1a4da632de222ef575906d39c5845fdd82
Austin <austinrdelgado@gmail.com>
2022-03-11 15:20:13 -0500

    Fixed broken break

--- main.js
+++ main.js
@@ -145,7 +145,7 @@ async function FloorGen()
             {
                 walkers.push(new Walker(currWalker.posX, currWalker.posY, Math.floor(Math.random() * 4)));
                 cellArray[currWalker.posY][currWalker.posX].classList.add("walker");
-                break;
+                return;
                 // Maybe limit this?
             }
         });
@@ -222,6 +222,7 @@ async function FloorGen()
             {
                 currWalker.posX--;
             }
+            
             // Add walker to current position
             cellArray[currWalker.posY][currWalker.posX].classList.add("walker");
         });
